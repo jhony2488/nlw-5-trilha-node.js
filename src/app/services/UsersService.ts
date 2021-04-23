76580b90b3bcfb5a6436f89480dda6c39e08ff0f
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-04-23 16:30:38 -0300

    services  user created

---
+++ src/app/services/UsersService.ts
@@ -0,0 +1,29 @@
+import { getCustomRepository, Repository } from 'typeorm'
+import { UserRepository } from '../repositories/UserRepository'
+import { User } from '../entities/User'
+
+interface InterfaceUsersService {
+    email: string
+}
+
+class UsersService {
+
+    async create({ email }: InterfaceUsersService) {
+        const usersRepository = getCustomRepository(UserRepository)
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email,
+        })
+        if (userAlreadyExists) {
+            return userAlreadyExists
+        }
+        const user = await usersRepository.create({
+            email,
+        })
+        await usersRepository.save(user)
+
+        return user
+    }
+}
+
+export { UsersService }
