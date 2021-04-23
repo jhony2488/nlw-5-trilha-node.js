6bc9ce315632d1aa4dea68b172a6b7926d8daacf
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-04-23 16:36:33 -0300

    config render and render template

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,23 @@
 import { Router } from 'express'
 import { SettingControllers } from './app/controllers/SettingsController'
+import { UserControllers } from './app/controllers/UsersControllers'
+import { MessageControllers } from './app/controllers/MessagesControllers'
 
 const settingsController = new SettingControllers()
+const usersController = new UserControllers()
+const messagesController = new MessageControllers()
 
 const routes = Router()
 
 //default
 routes.get('/', (req, res) => {
-    res.json({ version: '1.0.0' })
+    res.render('html/client.html')
 })
+
+routes.post('/users', usersController.create)
+
+routes.get('/messages/:user_id', messagesController.index)
+routes.post('/messages', messagesController.create)
 
 routes.post('/settings', settingsController.create)
 
