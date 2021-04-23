0f47c8b58fc1a93264e3b4a8da329c111d2a2c95
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-04-23 16:27:45 -0300

    add connection socket in app

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 export {}
 
 import express from 'express'
+import { createServer } from 'http'
+import { Server, Socket } from 'socket.io'
 import bodyParser from 'body-parser'
+import { routes } from './routes'
+import path from 'path'
 
 class AppController {
     public express
+    private http
+    public io
     constructor() {
         this.express = express()
 
+        this.http = createServer(this.express)
+        this.io = new Server(this.http)
+
+        this.websocket()
+        this.static()
         this.bodyParser()
         this.middleware()
         this.routes()
@@ -16,14 +27,27 @@ class AppController {
         this.express.use(express.json())
     }
     routes() {
-        this.express.use(require('./routes'))
+        this.express.use(routes)
     }
     bodyParser() {
         this.express.use(bodyParser.urlencoded({ extended: false }))
         this.express.use(bodyParser.json())
     }
+    static() {
+        this.express.use(express.static(path.join(__dirname, '../public')))
+        this.express.set('views', path.join(__dirname, '../public'))
+        this.express.engine('html', require('ejs').renderFile)
+        this.express.set('view engine', 'html')
+    }
+    websocket() {
+        this.io.on('connection', (client: Socket) => {
+            console.log('Conectado:' + client.id)
+        })
+    }
     server() {
-        this.express.listen(3000, () => console.log('servidor rodando'))
+        this.http.listen(80, () => {
+            console.log('servidor rodando')
+        })
     }
 }
 
