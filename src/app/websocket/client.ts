a9cb5c3a86bbfba94cf68101b5a60249d7b18e26
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-04-28 12:22:11 -0300

    connection client websocket created

--- src/app/websocket/client.ts
+++ src/app/websocket/client.ts
@@ -1,11 +1,49 @@
 export {}
 import { Socket } from 'socket.io'
+import { ConnectionsService } from '../services/ConnectionsService'
+import { UsersService } from '../services/UsersService'
+
 const app = require('../../app')
 
 const ioClient = app.io
 
 ioClient.on('connect', (client: Socket) => {
-    client.on('client_first_acess', (params) => {
-        console.log(params)
+    const connectionsService = new ConnectionsService()
+    const usersService = new UsersService()
+
+    client.on('client_first_acess', async (params) => {
+        const socket_id = client.id
+        const { text, email } = params
+
+        try {
+            const userExists = await usersService.findByEmail(email)
+
+            if (!userExists) {
+                console.log(email)
+
+                const user = await usersService.create({ email })
+
+                connectionsService.create({
+                    socket_id,
+                    user_id: user.id,
+                })
+            } else {
+                const connection = await connectionsService.findByUserId(
+                    userExists.id
+                )
+
+                if (connection) {
+                    connection.socket_id = socket_id
+                    connectionsService.create(connection)
+                } else {
+                    connectionsService.create({
+                        socket_id,
+                        user_id: userExists.id,
+                    })
+                }
+            }
+        } catch (err) {
+            console.log('err: ' + err)
+        }
     })
 })
