e73f92baa9f475255dabc6ae16f56c64059d1ca2
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-04-30 21:41:14 -0300

    routes created and configuration

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { PagesControllers } from './app/controllers/PagesControllers'
 import { SettingControllers } from './app/controllers/SettingsController'
 import { UserControllers } from './app/controllers/UsersControllers'
 import { MessageControllers } from './app/controllers/MessagesControllers'
@@ -6,19 +7,21 @@ import { MessageControllers } from './app/controllers/MessagesControllers'
 const settingsController = new SettingControllers()
 const usersController = new UserControllers()
 const messagesController = new MessageControllers()
+const pagesController = new PagesControllers()
 
 const routes = Router()
 
 //default
-routes.get('/', (req, res) => {
-    res.render('html/client.html')
-})
+routes.get('/', pagesController.index)
+routes.get('/admin', pagesController.admim)
+
+routes.get('/settings/:username', settingsController.findByUserName)
+routes.post('/settings', settingsController.create)
+routes.put('/settings/:username', settingsController.update)
 
 routes.post('/users', usersController.create)
 
 routes.get('/messages/:user_id', messagesController.index)
 routes.post('/messages', messagesController.create)
-
-routes.post('/settings', settingsController.create)
 
 export { routes }
